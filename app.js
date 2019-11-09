8bde30634124b2a0fd082c68afd4cc167c160c7b
pssingh21 <peeyushmansingh@gmail.com>
2019-11-09 16:42:26 +0100

    Initial committo

---
+++ app.js
@@ -0,0 +1,64 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+var http = require('http').createServer(app);
+var io = require('socket.io').listen(http);
+
+io.sockets.on('connection', (socket) => {
+  console.log('A user is connected');
+
+  socket.on('new-message', (message) => {
+    io.sockets.emit('new-message',message);
+  });
+
+  socket.on('disconnect', function(){
+    console.log("User disconnected");
+  });
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3030, function(){
+  console.log('listening on *:3030');
+});
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+module.exports = app;
